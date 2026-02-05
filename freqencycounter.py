d6111e49b707d2af25d09d72166c93e3a9171b08
Superdogz <lucasyyang2018@gmail.com>
2026-02-05 13:14:38 -0800

    Finished the freq counter

--- freqencycounter.py
+++ freqencycounter.py
@@ -1,5 +1,16 @@
-text = "lzwugvwogjvakjgddquzsaj"
+text = "lzwugvwogjvakjgddquzsaj" #change this
 
-print(chr(1))
-# for i in range(26):
-#     print(str(chr(i))+": "+str(text.count(chr(i))))
+count = {}
+percent = {}
+
+alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for letter in alpha:
+    count[letter] = text.count(letter)
+    percent[letter] = round(text.count(letter)/len(text),2)
+
+print("Letter count:")
+print(dict(sorted(count.items(), key=lambda x: x[1], reverse=True)))
+print()
+print("Letter percentage:")
+print(dict(sorted(percent.items(), key=lambda x: x[1], reverse=True)))
+    
