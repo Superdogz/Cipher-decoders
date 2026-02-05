478de4c112bc3dddd2970fdfcc1452ef4d25fc24
Superdogz <lucasyyang2018@gmail.com>
2026-02-05 13:19:25 -0800

    Integer percentages

--- freqencycounter.py
+++ freqencycounter.py
@@ -6,7 +6,7 @@ percent = {}
 alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u","v","w","x","y","z"]
 for letter in alpha:
     count[letter] = text.count(letter)
-    percent[letter] = round(text.count(letter)/len(text),2)
+    percent[letter] = int(text.count(letter)/len(text)*100)
 
 print("Letter count:")
 print(dict(sorted(count.items(), key=lambda x: x[1], reverse=True)))
