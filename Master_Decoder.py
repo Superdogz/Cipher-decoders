921276cb30132d7dfbea7193ccf9e448d7c9e8ec
Superdogz <lucasyyang2018@gmail.com>
2026-02-24 09:20:09 -0800

    Made the master cipher, still need to make transposition more readable and add the affine cipher.

---
+++ Master_Decoder.py
@@ -0,0 +1,67 @@
+import math
+
+alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def cesar_cipher(code):
+    for i in range(1,26): #go through each shift
+        shifted = ""
+
+        for l in code: #create the shifted string
+            shifted = shifted + alpha[(alpha.index(l)+i)%26]
+
+        with open("words.txt") as file: #check for valid words
+                for word in file:
+                    if word.strip() in shifted:
+                        print("Found Ceasar Shift at P=C+"+str(i)+" ("+word.strip()+")")
+                        print(shifted)
+                        print()
+                        break #only needs one recognized word
+
+# def transposition_cipher(code):
+#     tracker = list(code)
+
+#     for l in range(2,math.ceil(len(code)/2)+1):
+#         temp = tracker.copy()
+#         curr = 0
+#         decrypt = ""
+#         while len(temp) != 0:
+#             decrypt = decrypt + code[curr]
+#             temp.remove(curr)
+#             curr += l
+#             if curr >= len(code):
+#                 try:
+#                     curr = temp[0]
+#                 except:
+#                     break
+#         with open("words.txt") as file: #check for valid words
+#             for word in file:
+#                 if word.strip() in decrypt:
+#                     print("Found Ceasar Shift at P=C+"+str(i)+" ("+word.strip()+")")
+#                     print(decrypt)
+#                     print()
+#                     break #only needs one recognized word
+
+def multiplication_cipher(code):
+    mult_inverses = [1,9,21,15,3,19,7,23,11,5,17,25]
+    for m in mult_inverses:
+        multiplied = ""
+        for i in code:
+            multiplied = multiplied + alpha[(alpha.index(i)*m)%26]
+        with open("words.txt") as file: #check for valid words
+            for word in file:
+                if word.strip() in multiplied:
+                    print("Found Multiplication Cipher at P=C*"+str(m)+"; original was "+str(pow(m, -1, 26))+" ("+word.strip()+")")
+                    print(multiplied)
+                    print()
+                    break #only needs one recognized word but can trigger multiple times for different inverse values.
+
+
+
+if __name__ == "__main__":
+    # cipher_text = "yfcfqqmazhupqzfc" 
+    cipher_text = "vyml"
+    
+    cipher_text = cipher_text.lower()
+    cesar_cipher(cipher_text)
+    multiplication_cipher(cipher_text)
+ 
---
+++ decrypers/scytaletranspositioncipher.py
@@ -0,0 +1,42 @@
+import math
+code = 'sliiggmmaa'
+'sigmaligma'
+
+
+for space in range(len(code)):
+    if len(code) % space == 0: # find the factors to make a clean decryption
+        for i in range(space):
+            for j in range(len(code) // space):
+                
+
+
+
+
+
+
+# tracker = []
+# for i in range(len(code)):
+#     tracker.append(i)
+
+
+# for l in range(2,math.ceil(len(code)/2)+1):
+#     temp = tracker.copy()
+#     curr = 0
+#     decrypt = ""
+#     while len(temp) != 0:
+#         decrypt = decrypt + code[curr]
+#         temp.remove(curr)
+#         curr += l
+#         if curr >= len(code):
+#             try:
+#                 curr = temp[0]
+#             except:
+#                 break
+    # print(decrypt)
+    # print(l)
+    # print()
+
+
+        
+
+
